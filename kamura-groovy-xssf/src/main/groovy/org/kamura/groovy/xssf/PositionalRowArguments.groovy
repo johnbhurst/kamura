72f35174e8176774452ec83cdd7b5550f90118d7
John Hurst <john.b.hurst@gmail.com>
2016-09-22 11:47:07 +1200

    Moved cell type conversion methods to CellUtils.

--- kamura-groovy-xssf/src/main/groovy/org/kamura/groovy/xssf/PositionalRowArguments.groovy
+++ kamura-groovy-xssf/src/main/groovy/org/kamura/groovy/xssf/PositionalRowArguments.groovy
@@ -17,10 +17,16 @@
 package org.kamura.groovy.xssf
 
 import java.time.LocalDate
-import java.time.ZoneId
 
 import org.apache.poi.ss.usermodel.Cell
 import org.apache.poi.ss.usermodel.Row
+
+import static org.kamura.groovy.xssf.CellUtils.cellBigDecimal
+import static org.kamura.groovy.xssf.CellUtils.cellDate
+import static org.kamura.groovy.xssf.CellUtils.cellInteger
+import static org.kamura.groovy.xssf.CellUtils.cellLocalDate
+import static org.kamura.groovy.xssf.CellUtils.cellString
+import static org.kamura.groovy.xssf.CellUtils.cellInt
 
 class PositionalRowArguments implements RowArguments {
 
@@ -60,7 +66,7 @@ class PositionalRowArguments implements RowArguments {
       }
       switch (type) {
         case String: return cellString(cell)
-        case int: return makeInt(cell)
+        case int: return cellInt(cell)
         case Integer: return cellInteger(cell)
         case BigDecimal: return cellBigDecimal(cell)
         case Date: return cellDate(cell)
@@ -72,63 +78,5 @@ class PositionalRowArguments implements RowArguments {
     }
   }
 
-  static String cellString(Cell cell) {
-    switch (cell.cellType) {
-      case Cell.CELL_TYPE_NUMERIC: return numberToString(cell.numericCellValue)
-      case Cell.CELL_TYPE_STRING: return cell.stringCellValue
-      case Cell.CELL_TYPE_FORMULA: return cell.stringCellValue
-      case Cell.CELL_TYPE_BLANK: return ""
-      case Cell.CELL_TYPE_BOOLEAN: return cell.booleanCellValue ? "true" : "false"
-      case Cell.CELL_TYPE_ERROR: return "#ERROR: $cell.errorCellValue" // Possibly should throw exception
-      default: throw new IllegalArgumentException("Unsupported cell type $cell.cellType")
-    }
-  }
 
-  static String numberToString(double val) {
-    return Math.rint(val) == val ? (Math.round(val) as String) : (val as String)
-  }
-
-  static int makeInt(Cell cell) {
-    return Optional.ofNullable(cellInteger(cell)).orElseThrow {new IllegalArgumentException("Blank value")}
-  }
-
-  static Integer cellInteger(Cell cell) {
-    switch (cell.cellType) {
-      case Cell.CELL_TYPE_NUMERIC: return cell.numericCellValue as Integer
-      case Cell.CELL_TYPE_STRING: return cell.stringCellValue as Integer
-      case Cell.CELL_TYPE_FORMULA: return cell.numericCellValue as Integer
-      case Cell.CELL_TYPE_BLANK: return null
-      case Cell.CELL_TYPE_BOOLEAN: return cell.booleanCellValue ? 1 : 0
-      case Cell.CELL_TYPE_ERROR: throw new IllegalArgumentException("Error value")
-      default: throw new IllegalArgumentException("Unsupported cell type $cell.cellType")
-    }
-  }
-
-  static BigDecimal cellBigDecimal(Cell cell) {
-    switch (cell.cellType) {
-      case Cell.CELL_TYPE_NUMERIC: return cell.numericCellValue as BigDecimal
-      case Cell.CELL_TYPE_STRING: return cell.stringCellValue as BigDecimal
-      case Cell.CELL_TYPE_FORMULA: return cell.numericCellValue as BigDecimal
-      case Cell.CELL_TYPE_BLANK: return null
-      case Cell.CELL_TYPE_BOOLEAN: return cell.booleanCellValue ? 1 : 0
-      case Cell.CELL_TYPE_ERROR: throw new IllegalArgumentException("Error value")
-      default: throw new IllegalArgumentException("Unsupported cell type $cell.cellType")
-    }
-  }
-
-  static Date cellDate(Cell cell) {
-    switch (cell.cellType) {
-      case Cell.CELL_TYPE_NUMERIC: return cell.dateCellValue
-      case Cell.CELL_TYPE_STRING: throw new IllegalArgumentException("String value") // use cell formatting to convert?
-      case Cell.CELL_TYPE_FORMULA: return cell.dateCellValue
-      case Cell.CELL_TYPE_BLANK: return null
-      case Cell.CELL_TYPE_BOOLEAN: throw new IllegalArgumentException("Boolean value")
-      case Cell.CELL_TYPE_ERROR: throw new IllegalArgumentException("Error value")
-      default: throw new IllegalArgumentException("Unsupported cell type $cell.cellType")
-    }
-  }
-
-  static LocalDate cellLocalDate(Cell cell) {
-    return cellDate(cell)?.toInstant()?.atZone(ZoneId.systemDefault())?.toLocalDate()
-  }
 }
